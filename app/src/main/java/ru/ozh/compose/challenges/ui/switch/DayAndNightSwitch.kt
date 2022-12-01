97e61c18f299745197664376a4d65654b809dedd
Oleg <zhilo@surfstudio.ru>
2022-12-01 18:40:14 +0300

    get some refactors in DayAndNightSwitch.kt

--- app/src/main/java/ru/ozh/compose/challenges/ui/switch/DayAndNightSwitch.kt
+++ app/src/main/java/ru/ozh/compose/challenges/ui/switch/DayAndNightSwitch.kt
@@ -2,14 +2,14 @@ package ru.ozh.compose.challenges.ui.switch
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.TweenSpec
 import androidx.compose.foundation.background
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,10 +18,17 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.AnimationSpec
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.RippleRadius
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.SwitchHeight
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.ThumbStartOffset
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.SwitchWidth
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.ThumbDiameter
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.ThumbPathLength
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.Black
+import ru.ozh.compose.challenges.ui.switch.SwitchConsts.Orange
 import kotlin.math.roundToInt
 
 
@@ -30,14 +37,11 @@ fun SwitchDaN(
     checked: Boolean,
     onCheckedChange: ((Boolean) -> Unit)?,
     modifier: Modifier = Modifier,
-    thumbContent: (@Composable () -> Unit)? = null,
     enabled: Boolean = true,
-    //colors: SwitchColors = SwitchDefaults.colors(),
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 ) {
 
-    val thumbPaddingStart = SwitchStartOffset
-    val minBound = with(LocalDensity.current) { thumbPaddingStart.toPx() }
+    val minBound = with(LocalDensity.current) { ThumbStartOffset.toPx() }
     val maxBound = with(LocalDensity.current) { ThumbPathLength.toPx() }
     val valueToOffset = remember<(Boolean) -> Float>(minBound, maxBound) {
         { value -> if (value) maxBound else minBound }
@@ -46,11 +50,6 @@ fun SwitchDaN(
     val targetValue = valueToOffset(checked)
     val offset = remember { Animatable(targetValue) }
     val scope = rememberCoroutineScope()
-
-    SideEffect {
-        // min bound might have changed if the icon is only rendered in checked state.
-        offset.updateBounds(lowerBound = minBound)
-    }
 
     DisposableEffect(checked) {
         if (offset.targetValue != targetValue) {
@@ -61,20 +60,18 @@ fun SwitchDaN(
         onDispose { }
     }
 
-    // TODO: Add Swipeable modifier b/223797571
-    val toggleableModifier =
-        if (onCheckedChange != null) {
-            Modifier.toggleable(
-                value = checked,
-                onValueChange = onCheckedChange,
-                enabled = enabled,
-                role = Role.Switch,
-                interactionSource = interactionSource,
-                indication = null
-            )
-        } else {
-            Modifier
-        }
+    val toggleableModifier = if (onCheckedChange != null) {
+        Modifier.toggleable(
+            value = checked,
+            onValueChange = onCheckedChange,
+            enabled = enabled,
+            role = Role.Switch,
+            interactionSource = interactionSource,
+            indication = null
+        )
+    } else {
+        Modifier
+    }
 
     Box(
         modifier
@@ -84,67 +81,39 @@ fun SwitchDaN(
     ) {
         SwitchImpl(
             checked = checked,
-            enabled = enabled,
             thumbValue = offset.asState(),
             interactionSource = interactionSource,
             thumbShape = RoundedCornerShape(50),
-            minBound = thumbPaddingStart,
-            maxBound = ThumbPathLength,
-            thumbContent = thumbContent,
         )
     }
 }
 
 @Composable
-@Suppress("ComposableLambdaParameterNaming", "ComposableLambdaParameterPosition")
 private fun BoxScope.SwitchImpl(
     checked: Boolean,
-    enabled: Boolean,
     thumbValue: State<Float>,
-    thumbContent: (@Composable () -> Unit)?,
     interactionSource: InteractionSource,
     thumbShape: Shape,
-    minBound: Dp,
-    maxBound: Dp,
 ) {
-    val trackColor by animateColorAsState(if (checked) orange else black)
-    val isPressed by interactionSource.collectIsPressedAsState()
+    val trackColor by animateColorAsState(if (checked) Orange else Black)
 
-//    val thumbValueDp = with(LocalDensity.current) { thumbValue.value.toDp() }
-
-    val thumbSizeDp = ThumbDiameter
-
-    val thumbOffset = if (isPressed) {
-        with(LocalDensity.current) {
-            if (checked) {
-                ThumbPathLength
-            } else {
-                SwitchStartOffset
-            }.toPx()
-        }
-    } else {
-        thumbValue.value
-    }
-
-    val trackShape = RoundedCornerShape(50)
     val modifier = Modifier
         .align(Alignment.Center)
         .width(SwitchWidth)
         .height(SwitchHeight)
-        .background(trackColor, trackShape)
+        .background(trackColor, RoundedCornerShape(50))
 
     Box(modifier) {
-        val thumbColor = Color.White
         Box(
             modifier = Modifier
                 .align(Alignment.CenterStart)
-                .offset { IntOffset(thumbOffset.roundToInt(), 0) }
-               /* .indication(
+                .offset { IntOffset(thumbValue.value.roundToInt(), 0) }
+                .indication(
                     interactionSource = interactionSource,
-                    indication = rememberRipple(bounded = false, StateLayerSize / 2)
-                )*/
-                .requiredSize(thumbSizeDp)
-                .background(thumbColor, thumbShape),
+                    indication = rememberRipple(bounded = false, RippleRadius)
+                )
+                .requiredSize(ThumbDiameter)
+                .background(Color.White, thumbShape),
             contentAlignment = Alignment.Center
         ) {
 /*            if (thumbContent != null) {
@@ -157,20 +126,6 @@ private fun BoxScope.SwitchImpl(
         }
     }
 }
-
-internal val PressedHandleWidth = 28.0.dp
-internal val ThumbDiameter = 24.0.dp
-internal val TrackOutlineWidth = 2.0.dp
-internal val StateLayerSize = 40.0.dp
-private val SwitchStartOffset = 4.dp
-private val SwitchWidth = 60.0.dp
-private val SwitchHeight = 32.0.dp
-private val ThumbPadding = (SwitchHeight - ThumbDiameter) / 2
-private val ThumbPathLength = (SwitchWidth - ThumbDiameter) - ThumbPadding
-
-private val black = Color(red = 53, green = 53, blue = 53)
-private val orange = Color(red = 243, green = 109, blue = 24)
-private val AnimationSpec = TweenSpec<Float>(durationMillis = 100)
 
 @Preview(
     widthDp = 300,
---
+++ app/src/main/java/ru/ozh/compose/challenges/ui/switch/SwitchConsts.kt
@@ -0,0 +1,20 @@
+package ru.ozh.compose.challenges.ui.switch
+
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+internal object SwitchConsts {
+
+    val RippleRadius = 20.0.dp
+    val SwitchWidth = 60.0.dp
+    val SwitchHeight = 32.0.dp
+    val ThumbDiameter = 24.0.dp
+    val ThumbStartOffset = 4.dp
+    val ThumbPadding = (SwitchHeight - ThumbDiameter) / 2
+    val ThumbPathLength = (SwitchWidth - ThumbDiameter) - ThumbPadding
+
+    val Black = Color(red = 53, green = 53, blue = 53)
+    val Orange = Color(red = 243, green = 109, blue = 24)
+    val AnimationSpec = TweenSpec<Float>(durationMillis = 100)
+}
