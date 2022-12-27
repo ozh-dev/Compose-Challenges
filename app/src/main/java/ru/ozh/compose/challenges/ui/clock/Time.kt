9711c49a876cd3410ecb601824369eca18c6f18f
Oleg <zhilo@surfstudio.ru>
2022-12-27 22:45:54 +0300

    Created and apply time parts changes animation

--- app/src/main/java/ru/ozh/compose/challenges/ui/clock/Time.kt
+++ app/src/main/java/ru/ozh/compose/challenges/ui/clock/Time.kt
@@ -2,11 +2,14 @@ package ru.ozh.compose.challenges.ui.clock
 
 import java.util.*
 
-data class Time(val hours: Int, val minutes: Int, val seconds: Int)
-
-val calendar = Calendar.getInstance()
+data class Time(
+    val hours: Int,
+    val minutes: Int,
+    val seconds: Int
+)
 
 fun currentTime(): Time {
+    val calendar = Calendar.getInstance()
     return Time(
         hours = calendar.get(Calendar.HOUR_OF_DAY),
         minutes = calendar.get(Calendar.MINUTE),
